03d94665a2d7ccda7187d3836e6abe0b9ca5b30f
arsalanhub <arsalanmohd237@gmail.com>
2023-03-11 09:38:22 +0530

    Login Signup logic added

--- client/src/pages/LoginSignupPage.js
+++ client/src/pages/LoginSignupPage.js
@@ -1,13 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function LoginSignUpPage() {
   const navigate=useNavigate();
   const [isLogInPage, setIsLogInPage]=useState(false);
+  const [name, setName]=useState("");
+  const [email, setEmail]=useState("");
+  const [password, setPassword]=useState("");
 
   const loginSignUpText = (val) => {
     if(val) return "Login";
     return "Signup"
+  }
+
+  const clickHandler = async () => {
+    if(isLogInPage) {
+        let data = await axios.post("http://localhost:5000/login", {
+            email: email,
+            password: password
+        })
+        localStorage.setItem("userData", JSON.stringify(data.data));
+    } else {
+        let data = await axios.post("http://localhost:5000/signup", {
+            name: name, 
+            email: email,
+            password: password
+        })
+        localStorage.setItem("userData", JSON.stringify(data.data));
+    }
+    navigate("/dashboard");
   }
 
   useEffect(() => {
@@ -18,10 +40,10 @@ export default function LoginSignUpPage() {
   return (
     <>
        <div>{loginSignUpText(isLogInPage)} Page</div>
-       {!isLogInPage && <input type="name" placeholder='Enter Name' /> }
-       <input type="email" placeholder='Enter Email' />
-       <input type="password" placeholder='Enter Password' />
-       <button>{loginSignUpText(isLogInPage)}</button>
+       {!isLogInPage && <input type="name" placeholder='Enter Name' onChange={(e)=>setName(e.target.value)} /> }
+       <input type="email" placeholder='Enter Email' onChange={(e)=>setEmail(e.target.value)} />
+       <input type="password" placeholder='Enter Password' onChange={(e)=>setPassword(e.target.value)} />
+       <button onClick={()=>clickHandler()}>{loginSignUpText(isLogInPage)}</button>
        <a href="#" onClick={()=>setIsLogInPage(!isLogInPage)}>{loginSignUpText(!isLogInPage)}</a>
     </>
   )

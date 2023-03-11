c3c5c9daa28d05a21ae2344d63935d40e870e69a
arsalanhub <arsalanmohd237@gmail.com>
2023-03-11 15:24:16 +0530

    Dashboard UI design

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import UploadVideo from '../components/UploadVideo'
 import axios from "axios";
 import { useNavigate } from 'react-router-dom';
+import Avatar from '@mui/material/Avatar';
 import "../../src/App.css"
 
 export default function Dashboard() {
@@ -48,8 +49,14 @@ export default function Dashboard() {
       {videoData && videoData.length && videoData.map((data, idx) => {
         return (
             <div className='videoWrapper'>
-                <div>{getUserName(data.userId)}</div>
-                <video src={data.Url} key={data._id} height="350" width="600" controls></video>
+                <div style={{display: "flex", gap: "0.5rem", backgroundColor: "white", borderRadius: "1.5rem" }}>
+                    <Avatar sx={{ bgcolor: "purple" }}>OP</Avatar> 
+                    <div style={{ display: "flex", alignItems: "center" }}>{getUserName(data.userId)}</div>
+                </div>
+                <div style={{ display: "flex", gap: "0.5rem" }}>
+                    <video src={data.Url} key={data._id} height="350" width="600" controls style={{ borderRadius: "1.5rem" }}></video>
+                    <div style={{ backgroundColor: "white", marginTop: "0.25rem", width: "70%", borderRadius: "1.5rem", padding: "0.5rem" }}>Comments Section</div>
+                </div>
             </div>
         )
       })}

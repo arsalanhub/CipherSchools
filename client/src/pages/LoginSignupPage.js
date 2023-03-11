0f974d9e9d3897a18097a77c9368c0e2d56ed4c3
arsalanhub <arsalanmohd237@gmail.com>
2023-03-11 19:31:27 +0530

    Login & Signup Design

--- client/src/pages/LoginSignupPage.js
+++ client/src/pages/LoginSignupPage.js
@@ -39,30 +39,46 @@ export default function LoginSignUpPage() {
   }, []);
   return (
     <>
-      <div>{loginSignUpText(isLogInPage)} Page</div>
-      {!isLogInPage && (
-        <input
-          type="name"
-          placeholder="Enter Name"
-          onChange={(e) => setName(e.target.value)}
-        />
-      )}
-      <input
-        type="email"
-        placeholder="Enter Email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Enter Password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={() => clickHandler()}>
-        {loginSignUpText(isLogInPage)}
-      </button>
-      <a href="#" onClick={() => setIsLogInPage(!isLogInPage)}>
-        {loginSignUpText(!isLogInPage)}
-      </a>
+      <div className="loginSignupWrapper">
+        <div>{loginSignUpText(isLogInPage)} Page</div>
+        <div>
+          {!isLogInPage && (
+            <input
+              type="name"
+              placeholder="Enter Name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          )}
+        </div>
+        <div>
+          <input
+            type="email"
+            placeholder="Enter Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button onClick={() => clickHandler()}>
+          {loginSignUpText(isLogInPage)}
+        </button>
+        <div>
+          <a href="#" onClick={() => setIsLogInPage(!isLogInPage)}>
+            {loginSignUpText(!isLogInPage)}
+          </a>
+        </div>
+      </div>
+      <div className="description">
+        <div style={{ borderBottom: "1px solid" }}>Description</div>
+        <div>1. This is video sharing platform.</div>
+        <div>2. Here you can upload video, like & comment on video</div>
+        <div>3. If you are new then singup or use this credential to explore platform. <span style={{ fontWeight: "700" }}>(arsalan@gmail.com, 123456789)</span></div>
+      </div>
     </>
   );
 }
